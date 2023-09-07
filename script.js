bbe29a84e089bea5bb38e588fe5cf0f18b376dcf
Teun Jansen <teunjansen91@gmail.com>
2023-09-07 14:16:19 +0200

    localStorage()

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+// if the notes are there, they will be displayed when opening the browser:
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 //When the button is clicked, the commented p element is created with this function:
 createBtn.addEventListener("click", () => {
   let inputBox = document.createElement("p");
@@ -16,5 +26,15 @@ createBtn.addEventListener("click", () => {
 notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
     e.target.parentElement.remove();
+    updateStorage();
+  }
+  // updateStorage() everytime anything is typed into the p tag:
+  else if (e.target.tagName === "P") {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
   }
 });
